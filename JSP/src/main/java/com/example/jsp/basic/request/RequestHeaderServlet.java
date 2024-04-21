0eb4bc35e1f5a0f58395d7b6e6f02ecb20312443
acrnm148 <acrnm148@gmail.com>
2024-04-21 14:52:07 +0900

    feat(jsp) : HttpServletRequest 2024-04-21

---
+++ JSP/src/main/java/com/example/jsp/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,7 @@
+package com.example.jsp.basic.request;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet {
+}
